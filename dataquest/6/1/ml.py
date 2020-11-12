f629d44c7c7e9ed36ec3c3e47e2c9e50397b1456
igvel <igvel@mail.ru>
2020-11-12 17:22:58 +0300

    kNN Model Evaluation

--- dataquest/6/1/ml.py
+++ dataquest/6/1/ml.py
@@ -35,7 +35,7 @@ stripped_dollars = stripped_commas.str.replace('$', '')
 dc_listings['price'] = stripped_dollars.astype('float')
 dc_listings = dc_listings.loc[np.random.permutation(len(dc_listings))]
 
-# Predict price
+# Predict price based on accomodates
 def predict_price(new_listing):
     temp_df = dc_listings.copy()
     temp_df['distance'] = (temp_df['accommodates'] - new_listing).apply(abs)
@@ -45,3 +45,52 @@ def predict_price(new_listing):
 acc_one = predict_price(1)
 acc_two = predict_price(2)
 acc_four = predict_price(4)
+
+# Model validation via train/test sets
+train_df = dc_listings.iloc[0:2792]
+test_df = dc_listings.iloc[2792:]
+
+def predict_price(new_listing):
+    ## DataFrame.copy() performs a deep copy
+    temp_df = train_df.copy()
+    temp_df['distance'] = temp_df['accommodates'].apply(lambda x: np.abs(x - new_listing))
+    temp_df = temp_df.sort_values('distance')
+    nearest_neighbor_prices = temp_df.iloc[0:5]['price']
+    predicted_price = nearest_neighbor_prices.mean()
+    return(predicted_price)
+
+test_df["predicted_price"] = test_df["accommodates"].apply(predict_price)
+
+# Mean absolute error
+mae = np.absolute(test_df["price"] - test_df["predicted_price"]).mean()
+# Mean squared error
+mse = np.square(np.absolute(test_df["price"] - test_df["predicted_price"])).mean()
+
+# Model built on bathrooms
+train_df = dc_listings.iloc[0:2792]
+test_df = dc_listings.iloc[2792:]
+
+def predict_price(new_listing):
+    temp_df = train_df.copy()
+    temp_df['distance'] = temp_df['bathrooms'].apply(lambda x: np.abs(x - new_listing))
+    temp_df = temp_df.sort_values('distance')
+    nearest_neighbors_prices = temp_df.iloc[0:5]['price']
+    predicted_price = nearest_neighbors_prices.mean()
+    return(predicted_price)
+
+test_df["predicted_price"] = test_df["bathrooms"].apply(predict_price)
+
+squared_error = np.square(numpy.absolute(test_df["price"] - test_df["predicted_price"]))
+# Mean squared error
+mse = squared_error.mean()
+# Root mean square error
+rmse = np.sqrt(mse)
+
+# Looking at the ratio of MAE to RMSE can help us understand if there are large but infrequent errors.
+errors_one = pd.Series([5, 10, 5, 10, 5, 10, 5, 10, 5, 10, 5, 10, 5, 10, 5, 10, 5, 10])
+errors_two = pd.Series([5, 10, 5, 10, 5, 10, 5, 10, 5, 10, 5, 10, 5, 10, 5, 10, 5, 1000])
+
+mae_one = errors_one.mean()
+mae_two = errors_two.mean()
+rmse_one = (errors_one**2).mean()**(1/2)
+rmse_two = (errors_two**2).mean()**(1/2)
