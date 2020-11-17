6309f866d0442f2a277d4aa4663f10c97ad9a743
igvel <igvel@mail.ru>
2020-11-17 23:07:42 +0300

    Gradient descent and OLS fitting methods

--- dataquest/6/4/linreg.py
+++ dataquest/6/4/linreg.py
@@ -139,3 +139,93 @@ test_rmse_2 = np.sqrt(test_mse)
 
 print(train_rmse_2)
 print(test_rmse_2)
+
+# Use Gradient Descent to optimize function param a1
+def derivative(a1, xi_list, yi_list):
+    len_data = len(xi_list)
+    error = 0
+    for i in range(0, len_data):
+        error += xi_list[i]*(a1*xi_list[i] - yi_list[i])
+    deriv = 2*error/len_data
+    # Single line variant
+    # deriv = (2/len(xi_list))*(xi_list*(xi_list*a1 - yi_list)).sum()
+    return deriv
+
+def gradient_descent(xi_list, yi_list, max_iterations, alpha, a1_initial):
+    a1_list = [a1_initial]
+
+    for i in range(0, max_iterations):
+        a1 = a1_list[i]
+        deriv = derivative(a1, xi_list, yi_list)
+        a1_new = a1 - alpha*deriv
+        a1_list.append(a1_new)
+    return(a1_list)
+
+param_iterations = gradient_descent(train['Gr Liv Area'], train['SalePrice'], 20, .0000003, 150)
+final_param = param_iterations[-1]
+
+# Gradient descent for two parameters
+def a1_derivative(a0, a1, xi_list, yi_list):
+    len_data = len(xi_list)
+    error = 0
+    for i in range(0, len_data):
+        error += xi_list[i]*(a0 + a1*xi_list[i] - yi_list[i])
+    deriv = 2*error/len_data
+    return deriv
+
+def a0_derivative(a0, a1, xi_list, yi_list):
+    len_data = len(xi_list)
+    error = 0
+    for i in range(0, len_data):
+        error += a0 + a1*xi_list[i] - yi_list[i]
+    deriv = 2*error/len_data
+    return deriv
+
+def gradient_descent(xi_list, yi_list, max_iterations, alpha, a1_initial, a0_initial):
+    a1_list = [a1_initial]
+    a0_list = [a0_initial]
+
+    for i in range(0, max_iterations):
+        a1 = a1_list[i]
+        a0 = a0_list[i]
+
+        a1_deriv = a1_derivative(a0, a1, xi_list, yi_list)
+        a0_deriv = a0_derivative(a0, a1, xi_list, yi_list)
+
+        a1_new = a1 - alpha*a1_deriv
+        a0_new = a0 - alpha*a0_deriv
+
+        a1_list.append(a1_new)
+        a0_list.append(a0_new)
+    return(a0_list, a1_list)
+
+a0_params, a1_params = gradient_descent(train['Gr Liv Area'], train['SalePrice'], 20, .0000003, 150, 1000)
+print(a0_params)
+print(a1_params)
+
+# Calculating OLS - ordinary least squares estimation for parameters
+data = pd.read_csv('AmesHousing.txt', delimiter="\t")
+train = data[0:1460]
+test = data[1460:]
+
+features = ['Wood Deck SF', 'Fireplaces', 'Full Bath', '1st Flr SF', 'Garage Area',
+       'Gr Liv Area', 'Overall Qual']
+
+X = train[features]
+X['bias'] = 1
+X = X[['bias'] + features]
+y = train['SalePrice']
+
+# Formula: ols = inv(transX*X)*transX*y
+first_term = np.linalg.inv(
+        np.dot(
+            np.transpose(X),
+            X
+        )
+    )
+second_term = np.dot(
+        np.transpose(X),
+        y
+    )
+ols_estimation = np.dot(first_term, second_term)
+print(ols_estimation)
