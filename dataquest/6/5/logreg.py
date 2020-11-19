e48aafe7ab84e8bc745d177a0e8864ce3be57976
igvel <igvel@mail.ru>
2020-11-19 20:14:21 +0300

    Logistic regression finished - evaluation, multi-class classification

--- dataquest/6/5/logreg.py
+++ dataquest/6/5/logreg.py
@@ -40,4 +40,101 @@ plt.scatter(admissions["gpa"], pred_probs[:,1])
 # Predict label - 0 or 1
 fitted_labels = logistic_model.predict(admissions[["gpa"]])
 
-plt.scatter(admissions["gpa"], fitted_labels)#
+plt.scatter(admissions["gpa"], fitted_labels)
+
+admissions = pd.read_csv("admissions.csv")
+model = LogisticRegression()
+model.fit(admissions[["gpa"]], admissions["admit"])
+
+labels = model.predict(admissions[["gpa"]])
+admissions["predicted_label"] = labels
+print(admissions["predicted_label"].value_counts())
+print(admissions.head())
+
+# Calculate accuracy of prediction
+admissions["actual_label"] = admissions["admit"]
+matches = admissions["predicted_label"] == admissions["actual_label"]
+correct_predictions = admissions[matches]
+print(correct_predictions.head())
+accuracy = len(correct_predictions) / len(admissions)
+print(accuracy)
+
+# True positives and negatives
+true_positive_filter = (admissions["predicted_label"] == 1) & (admissions["actual_label"] == 1)
+true_positives = len(admissions[true_positive_filter])
+
+true_negative_filter = (admissions["predicted_label"] == 0) & (admissions["actual_label"] == 0)
+true_negatives = len(admissions[true_negative_filter])
+
+print(true_positives)
+print(true_negatives)
+
+# Calculating sensitivity
+true_positive_filter = (admissions["predicted_label"] == 1) & (admissions["actual_label"] == 1)
+true_positives = len(admissions[true_positive_filter])
+false_negative_filter = (admissions["predicted_label"] == 0) & (admissions["actual_label"] == 1)
+false_negatives = len(admissions[false_negative_filter])
+
+sensitivity = true_positives / (true_positives + false_negatives)
+
+print(sensitivity)
+
+# Calculating specificity
+true_positive_filter = (admissions["predicted_label"] == 1) & (admissions["actual_label"] == 1)
+true_positives = len(admissions[true_positive_filter])
+false_negative_filter = (admissions["predicted_label"] == 0) & (admissions["actual_label"] == 1)
+false_negatives = len(admissions[false_negative_filter])
+true_negative_filter = (admissions["predicted_label"] == 0) & (admissions["actual_label"] == 0)
+true_negatives = len(admissions[true_negative_filter])
+false_positive_filter = (admissions["predicted_label"] == 1) & (admissions["actual_label"] == 0)
+false_positives = len(admissions[false_positive_filter])
+specificity = (true_negatives) / (false_positives + true_negatives)
+print(specificity)
+
+## Multi-class classification
+cars = pd.read_csv("auto.csv")
+print(cars.head())
+unique_origins = cars["origin"].unique()
+print(unique_origins)
+
+# Create dummy columns for categorical variables
+dummy_cylinders = pd.get_dummies(cars["cylinders"], prefix="cyl")
+cars = pd.concat([cars, dummy_cylinders], axis=1)
+dummy_years = pd.get_dummies(cars["year"], prefix="year")
+cars = pd.concat([cars, dummy_years], axis=1)
+cars = cars.drop("year", axis=1)
+cars = cars.drop("cylinders", axis=1)
+print(cars.head())
+
+# Split into train and test
+shuffled_rows = np.random.permutation(cars.index)
+shuffled_cars = cars.iloc[shuffled_rows]
+highest_train_row = int(cars.shape[0] * .70)
+train = shuffled_cars.iloc[0:highest_train_row]
+test = shuffled_cars.iloc[highest_train_row:]
+
+# Create N binary models for each class
+models = {}
+features = [c for c in train.columns if c.startswith("cyl") or c.startswith("year")]
+
+for origin in unique_origins:
+    model = LogisticRegression()
+
+    X_train = train[features]
+    y_train = train["origin"] == origin
+
+    model.fit(X_train, y_train)
+    models[origin] = model
+
+# Create data frame with probabilities for each model
+testing_probs = pd.DataFrame(columns=unique_origins)
+
+for origin in unique_origins:
+    # Select testing features.
+    X_test = test[features]
+    # Compute probability of observation being in the origin.
+    testing_probs[origin] = models[origin].predict_proba(X_test)[:,1]
+
+# Select maximum probability to predict label
+predicted_origins = testing_probs.idxmax(axis=1)
+print(predicted_origins)
